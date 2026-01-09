1356d03581bceb34343ca9df3549cbfdcc9d89d7
akmaster <akmaster1991@gmail.com>
2026-01-10 01:22:14 +0300

    feat: enforce admin privileges via winres manifest

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,3 +1,9 @@
 fn main() {
+    #[cfg(windows)]
+    {
+        let mut res = winres::WindowsResource::new();
+        res.set_manifest_file("app.manifest");
+        res.compile().unwrap();
+    }
     tauri_build::build()
 }
